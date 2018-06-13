1b15c7b3c50d4c414a6a02017fe76c4020a63479
rgutfinger <gutfinger@cox.net>
2018-06-13 14:45:05 -0700

    .

--- ExrWeb/App_Start/BundleConfig.cs
+++ ExrWeb/App_Start/BundleConfig.cs
@@ -13,6 +13,12 @@ namespace ExrWeb
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 				"~/Scripts/jquery-{version}.js"));
 
+
+				bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+					"~/Scripts/jquery-ui.js"));
+
+
+
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 				"~/Scripts/jquery.unobtrusive*",
 				"~/Scripts/jquery.validate*"));
@@ -40,6 +46,7 @@ namespace ExrWeb
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 				 "~/Content/bootstrap.css",
+						"~/Content/jquery-ui.css",
 				 "~/Content/Site.css"));
 		}
 	}
