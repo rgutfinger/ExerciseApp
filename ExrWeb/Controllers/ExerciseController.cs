dbd4547e677fe9b165cce93de292a81f8d687d02
rgutfinger <gutfinger@cox.net>
2018-06-14 10:43:29 -0700

    .

--- ExrWeb/Controllers/ExerciseController.cs
+++ ExrWeb/Controllers/ExerciseController.cs
@@ -7,15 +7,15 @@ using System.Web.Mvc;
 
 namespace ExrWeb.Controllers
 {
-    public class ExerciseController : Controller
-    {
+	public class ExerciseController : Controller
+	{
 		static List<Exercise> m_elist = new List<Exercise>();
 
 		// GET: Exercise
 		public ActionResult Index()
-        {
-            return View(new Exercise());
-        }
+		{
+			return View(new Exercise());
+		}
 
 		[HttpPost]
 		public ActionResult Add(Exercise model)
@@ -30,9 +30,9 @@ namespace ExrWeb.Controllers
 
 			if (string.IsNullOrWhiteSpace(model.Machine))
 				ModelState.AddModelError("Machine", "Machine name missing");
-			if ( model.NumReps<=0 )
+			if (model.NumReps <= 0)
 				ModelState.AddModelError("NumReps", "NumReps must be positive");
-			if ( model.ExDate<=new DateTime(2018,1,1) )
+			if (model.ExDate <= new DateTime(2018, 1, 1))
 				ModelState.AddModelError("ExDate", "Invalid date");
 
 			if (ModelState.IsValid)
@@ -52,14 +52,32 @@ namespace ExrWeb.Controllers
 		{
 			return View(m_elist);
 		}
-		
+
 		public ActionResult Delete(string machine)
 		{
 			Exercise ex = m_elist.Find(e => e.Machine.Equals(machine, StringComparison.InvariantCultureIgnoreCase));
 			m_elist.Remove(ex);
 
-			return View("List",m_elist);
+			return View("List", m_elist);
 		}
 
+		public ActionResult Edit(string machine)
+		{
+			Exercise ex = m_elist.Find(e => e.Machine.Equals(machine, StringComparison.InvariantCultureIgnoreCase));
+			//m_elist.Remove(ex);
+
+			//return View("Edit", ex);
+			return View("Edit", ex);
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Exercise model)
+		{
+			Exercise ex = m_elist.Find(e => e.Machine.Equals(model.Machine, StringComparison.InvariantCultureIgnoreCase));
+			ex.NumReps = model.NumReps;
+			ex.ExDate = model.ExDate;
+
+			return View("List", m_elist);
+		}
 	}
 }
