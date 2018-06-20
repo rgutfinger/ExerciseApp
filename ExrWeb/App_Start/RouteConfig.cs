4ea0ef432876a51a1a2935e74ef0c880148d9842
rgutfinger <gutfinger@cox.net>
2018-06-20 14:52:55 -0700

    set default controller as Exercise (not Home)

--- ExrWeb/App_Start/RouteConfig.cs
+++ ExrWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace ExrWeb
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				//defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Exercise", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
